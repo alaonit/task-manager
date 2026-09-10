29a26f4cf1cd203b9fc43d713faa652ee33a12be
Alaonit <alagiesaikou77@gmail.com>
2026-09-10 23:59:50 +0000

    fix: correct form submission handling and improve structure

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,24 @@
-
-
-function Form(){
-
-  function submit(formData){
-    const task = FormData.get()
+function Form() {
+  function submit(formData) {
+    const task = formData.get("taskName");
+    console.log(task);
   }
-  return(
-    <form action={submit}>
-      <label htmlFor="">Task</label>
-      <input id="taskName" type="text" name="text" />
+  return (
+    <form className="bg-amber-200 flex flex-col" action={submit}>
+      <div>
+        <label htmlFor="taskName">Task</label>
+        <input id="taskName" type="text" name="textName" />
 
-      <label htmlFor="">Task</label>
-      <input id="text" type="text" name="text" />
+        <label htmlFor="">Task</label>
+        <input id="text" type="text" name="text" />
 
-      <label htmlFor="description">Description</label>
-      <textarea name="description" id="description"></textarea>
+        <label htmlFor="description">Description</label>
+        <textarea name="description" id="description"></textarea>
+
+        <button>Submit</button>
+      </div>
     </form>
-  )
+  );
 }
 
 export default Form;
