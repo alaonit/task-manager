cd75287a72e1f1caffda938539b760c7e9fb407a
Alaonit <alagiesaikou77@gmail.com>
2026-09-11 00:00:04 +0000

    refactor: simplify App component by removing unnecessary markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-
+import Form from "./components/Form.jsx"
 
 function App(){
   return(
     <main>
-       <div className="min-h-screen bg-gray-100 p-10">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Task Manager
-      </h1>
-    </div>
+       <Form/>
     </main>
   )
 }
